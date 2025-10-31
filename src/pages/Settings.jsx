d47ec57d0f909f1700df612248eb67e008145515
Coderbarkha <barkhathakkar241@gmail.com>
2025-10-31 22:50:08 +0530

    Remove unused Bootstrap imports and add dark mode styles

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./dark-mode.css"; // your own CSS file
 
 const Settings = () => {
